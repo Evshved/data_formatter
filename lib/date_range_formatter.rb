cb5cfca89cecd79c9e29f4a7ff46c63fe28ea53b
Eugene Shved <shved.eugene@gmail.com>
2021-09-15 02:35:52 +0300

    Update data range formatter

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -1,14 +1,16 @@
-require "date"
-require "fixnum"
+# frozen_string_literal: true
+
+require 'date'
+require 'fixnum'
 
 class DateRangeFormatter
-  attr_reader :start_date, :end_date, :start_time, :end_time
-
-  HUMANISE_CONFIG = {
+  TIME_CONFIG_PATTERNS = {
     date_format: '%%s %B %Y',
     time_prefix: 'at',
     time_format: '%H:%M'
   }.freeze
+
+  attr_reader :start_time, :end_time, :start_date, :end_date
 
   def initialize(start_date, end_date, start_time = nil, end_time = nil)
     @start_date = parse_date(start_date)
@@ -18,88 +20,86 @@ class DateRangeFormatter
   end
 
   def to_s
-    return humanise_same_day_range if same_day?
-    return humanise_same_month_range if same_month?
-    return humanise_same_year_range if same_year?
-    humanise_date_range
+    return prepare_day_range if equal_day?
+    return prepare_month_range if equal_month?
+    return prepare_year_range if equal_year?
+
+    perform_date_range
   end
 
-  def same_day?
+  def equal_day?
     start_date == end_date
   end
 
-  def same_month?
+  def equal_month?
     start_date.month == end_date.month && start_date.year == end_date.year
   end
 
-  def same_year?
+  def equal_year?
     start_date.year == end_date.year
   end
 
   private
 
   def parse_date(date)
-    binding.
-    Date.iso8601(time)
     Date.parse(date)
   end
 
   def parse_time(time)
-    return nil if time.nil?
-    binding.irb
-    Time.iso8601
-    Date.iso8601(time)
-    Time.parse(time)
+    return nil unless time
+
+    DateTime.parse(time)
   end
 
-  def humanise_date(date, config)
-    config = HUMANISE_CONFIG.merge(config)
+  def perform_date(date, config)
+    config = TIME_CONFIG_PATTERNS.merge(config)
     return date.day.ordinalize unless config[:date_format]
+
     date.strftime(config[:date_format]) % date.day.ordinalize
   end
 
-  def humanise_time(time, config)
-    config = HUMANISE_CONFIG.merge(config)
+  def perform_time(time, config)
+    config = TIME_CONFIG_PATTERNS.merge(config)
     return '' unless time
+
     "#{config[:time_prefix]} #{time.strftime(config[:time_format])}".strip
   end
 
-  def humanise_date_time(type, config = {})
-    config = HUMANISE_CONFIG.merge(config)
-    human_date = humanise_date(send("#{type}_date".to_sym), config)
-    human_time = humanise_time(send("#{type}_time".to_sym), config)
+  def perform_date_time(type, config = {})
+    config = TIME_CONFIG_PATTERNS.merge(config)
+    human_date = perform_date(send("#{type}_date".to_sym), config)
+    human_time = perform_time(send("#{type}_time".to_sym), config)
     "#{human_date} #{human_time}".strip
   end
 
-  def humanise_date_range
-    "#{humanise_date_time('start')} - #{humanise_date_time('end')}".strip
+  def perform_date_range
+    "#{perform_date_time('start')} - #{perform_date_time('end')}".strip
   end
 
-  def humanise_same_year_range
-    start_time_prefix = if start_time || end_time
-                          humanise_date_time('start')
+  def prepare_year_range
+    start_time_starts = if start_time || end_time
+                          perform_date_time('start')
                         else
-                          humanise_date(start_date, date_format: '%%s %B')
+                          perform_date(start_date, date_format: '%%s %B')
                         end
-    "#{start_time_prefix} - #{humanise_date_time('end')}".strip
+    "#{start_time_starts} - #{perform_date_time('end')}".strip
   end
 
-  def humanise_same_month_range
-    start_time_prefix = if start_time || end_time
-                          humanise_date_time('start')
+  def prepare_month_range
+    start_time_starts = if start_time || end_time
+                          perform_date_time('start')
                         else
-                          humanise_date(start_date, date_format: nil)
+                          perform_date(start_date, date_format: nil)
                         end
-    "#{start_time_prefix} - #{humanise_date_time('end')}".strip
+    "#{start_time_starts} - #{perform_date_time('end')}".strip
   end
 
-  def humanise_same_day_range
-    end_time_suffix = if start_time
-                        humanise_time(end_time, time_prefix: 'to')
-                      else
-                        humanise_time(end_time, time_prefix: 'until')
+  def prepare_day_range
+    end_time_ends = if start_time
+                      perform_time(end_time, time_prefix: 'to')
+                    else
+                      perform_time(end_time, time_prefix: 'until')
                       end
-    "#{humanise_date_time('start')} #{end_time_suffix}".strip
+    "#{perform_date_time('start')} #{end_time_ends}".strip
   end
 end
-
