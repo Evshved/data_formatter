ccfaf4991b57b9555f22c83088804583544ba54c
Eugene Shved <shved.eugene@gmail.com>
2021-09-15 02:29:36 +0300

    Update fixnum

--- lib/fixnum.rb
+++ lib/fixnum.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class Fixnum
   def ordinalize
     if (11..13).include?(abs % 100)
       "#{self}th"
     else
       case abs % 10
-      when 1; "#{self}st"
-      when 2; "#{self}nd"
-      when 3; "#{self}rd"
-      else    "#{self}th"
+      when 1 then "#{self}st"
+      when 2 then "#{self}nd"
+      when 3 then "#{self}rd"
+      else "#{self}th"
       end
     end
   end
 
   def ordinalize
-    
     return "#{self}th" if (11..13).cover?(abs % 100)
+
     case abs % 10
     when 1 then "#{self}st"
     when 2 then "#{self}nd"
